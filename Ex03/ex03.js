0097135bbc7d132c4ec11f62c24abaedddf974cf
Freitas Guaglianone <diegofguaglianone@gmail.com>
2024-03-10 18:30:44 -0300

    Ex 03 concluído

---
+++ Ex03/ex03.js
@@ -0,0 +1,6 @@
+// * a) 9 (números ímpares)
+// * b) 128 (multiplicar os valores por 2)
+// * c) 49 (somar o número pelos ímpares consequentes 1+3=4 4+5=9 9+7=16...)
+// * d) 100 (são quadrados perfeitos 2^2=4 4^2=16...)
+// * e) 13 (sequencia de fibonacci)
+// * f) 20 (não consegui identificar nenhum padrão matemático)
