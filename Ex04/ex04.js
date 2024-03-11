33ab6bb30ca90ea317e63a7c41e4ebacfeee4abc
Freitas Guaglianone <diegofguaglianone@gmail.com>
2024-03-10 22:13:57 -0300

    Ex 04 concluído (apenas comentários)

---
+++ Ex04/ex04.js
@@ -0,0 +1,21 @@
+/*
+ * Considerando as salas A, B e C e as lâmpadas 1, 2 e 3:
+ * 
+ * Ligaria o interruptor 1 por um tempo e depois desligaria
+ * 
+ * Depois de um tempo ligaria o interruptor 2 e visitaria a sala A
+ * 
+ * SE a sala A estiver acesa 2 = A
+ * Então iria para a sala B, verificar a temperatura da lampada
+ * se estivesse quente então 1 = B e 3 = C
+ * se ela estivesse fria então 1 = C e 3 = B
+ * 
+ * 
+ * se ela estiver apagada, eu tocaria a lâmpada para verificar se está quente.
+ * Caso esteja 1 = A; se ela estiver fria então 3 = A;
+ * Iria para a próxima sala, se a sala B estivesse acesa então 2 = B 
+ * se estivesse apagada 2 = C 
+ * e a opção que restar da primeira sala, será a sala B
+ * 
+ *
+*/ 
